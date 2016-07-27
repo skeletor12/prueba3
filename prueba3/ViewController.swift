4cfa759b44fb03e0359643d6375b6dce88e6b798
luis rodriguez <betowas12345@hotmail.com>
2016-07-27 15:37:05 -0500

    Add files via upload

--- prueba3/ViewController.swift
+++ prueba3/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var tamaño2 = ""
+    @IBOutlet weak var chica: UIButton!
+    @IBOutlet weak var siguiente: UIButton!
+    @IBOutlet weak var mediana: UIButton!
+    @IBOutlet weak var grande: UIButton!
     
-    @IBOutlet weak var tamaño: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +34,38 @@ class ViewController: UIViewController {
     }
 
     func imprime()->String{
-        var textam = String(self.tamaño.text!)
+        var textam = String(tamaño2)
         
         if textam == "" {
             textam = "NO ELEGISTE TAMAÑO"
         }
         
         return textam
+    }
+    
+    
+    @IBAction func chicaSelected(sender: AnyObject) {
+        self.tamaño2 =  "CHICA"
+        chica.backgroundColor = UIColor.blackColor()
+        mediana.backgroundColor = UIColor.clearColor()
+        grande.backgroundColor = UIColor.clearColor()
+    }
+    
+
+    @IBAction func medianaSelected(sender: AnyObject) {
+        self.tamaño2 = "MEDIANA"
+        mediana.backgroundColor = UIColor.blackColor()
+        chica.backgroundColor = UIColor.clearColor()
+        grande.backgroundColor = UIColor.clearColor()
+    }
+    
+    
+    
+    @IBAction func grandeSelected(sender: AnyObject) {
+        self.tamaño2 = "GRANDE"
+        grande.backgroundColor = UIColor.blackColor()
+        chica.backgroundColor = UIColor.clearColor()
+        mediana.backgroundColor = UIColor.clearColor()
     }
     
     
